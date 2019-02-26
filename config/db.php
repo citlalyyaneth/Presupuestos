e5d9c498219dc97d4bdfbe1f47c691dc87018e33
citlalyyaneth <1630035@upv.edu.mx>
2019-02-26 05:18:38 +0000

    presupuestos

---
+++ config/db.php
@@ -0,0 +1,11 @@
+<?php
+	/*-------------------------
+	Autor: Obed Alvarado
+	Web: obedalvarado.pw
+	Mail: info@obedalvarado.pw
+	---------------------------*/
+define("DB_HOST", "localhost");//DB_HOST:  generalmente suele ser "127.0.0.1"
+define("DB_NAME", "presupuestos");//Nombre de la base de datos
+define("DB_USER", "admin");//Usuario de tu base de datos
+define("DB_PASS", "bbb3cdeef4e94bbe91d306046c66afc4618c096effccba30");//Contraseña del usuario de la base de datos
+
